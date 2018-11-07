510f0c08d760b64c23951d125ad2b80f0be766f6
wojciech-jakub <wojciech.jargielo@gmail.com>
2018-11-07 18:50:37 +0100

    json

--- test.py
+++ test.py
@@ -29,43 +29,33 @@ def get_time_seconds():
     return(int(round(time.time() * 1000000)))
 
 def save_db(data):
-    ref = db.reference('server/saving-data/fireblog')
-    users_ref = ref.child('users')
+    ref = db.reference('data/')
+    users_ref = ref.child('measurement')
     users_ref.set(data)
 
 def print_values(values):
     x = {
-	"timestamp" : time.time(),
-	"measurement" : {
-		"analog":{
-				"temperature" : values[0]	
-			},
-		"bm280" :{
-				"atlitude": values[1],
-				"temperature": values[4],
-				"pressure": values[6]
-			},
-		"dht22" :{
-				"temperature":values[3],
-				"humidity":values[2]
-			},
-		"gy30" :{
-				"lux": values[5]
-			},
-	}
+	str.(time.time()) : {
+            "analog":{
+                    "temperature" : values[0]
+                },
+            "bm280" :{
+                    "atlitude": values[1],
+                    "temperature": values[4],
+                    "pressure": values[6]
+                },
+            "dht22" :{
+                    "temperature":values[3],
+                    "humidity":values[2]
+                },
+            "gy30" :{
+                    "lux": values[5]
+                },
+            "timestamp" : time.time()
+        }
     }
-    save_db(x);
+    save_db(x)
     print(values)
-    #print("\n------ MEASUREMENT ------")
-    #print("TEMP Analog: ", values[1],"\n")
-    #print("BMP Atlitude: ", values[1:2],"\n")
-    #print("Humidity 22: ", values[1:3],"\n")
-    #print("temp22: ", values[3:4],"\n")
-    #print("BMP TEMP: ", values[3:5],"\n")
-    #print("LUX: ", values[3:6], "\n")
-   # print(values[0:3])
-   # print(values[3:6])
-#    print(values[6:9])
 
 
 class ReadFromArduino(object):
@@ -105,18 +95,15 @@ class ReadFromArduino(object):
                     read = True
 
                     self.latest_values = np.array(new_values)
-                   
+
                     if self.verbose > 1:
                         print("Time elapsed since last (ms): " + str(time_elapsed))
                         print_values(new_values)
-			
                     return(True)
 
         return(False)
 
 
-################################################################################
-# use is:
 ports = look_for_available_ports()
 usb_port = serial.Serial('/dev/ttyUSB0', baudrate=9600)
 read_from_Arduino_instance = ReadFromArduino(usb_port, verbose=6)
@@ -126,21 +113,4 @@ while True:
     read_from_Arduino_instance.read_one_value()
 
 
-"""
-################################################################################
-# measure noise of sensor (for LSM9DS0)
-ports = look_for_available_ports()
-usb_port = serial.Serial(ports[0], baudrate=115200, timeout=0.5)
-read_from_Arduino_instance = ReadFromArduino(usb_port, verbose=0)
-nbr_measurements = 1000
-matrix_measurements = np.zeros((nbr_measurements, 6))
-for ind in range(nbr_measurements):
-    read_from_Arduino_instance.read_one_value()
-    matrix_measurements[ind, :] = read_from_Arduino_instance.latest_values
-for ind in range(6):
-    crrt_array = matrix_measurements[:, ind]
-    print(np.std(crrt_array)**2)
-for ind in range(6):
-    crrt_array = matrix_measurements[:, ind]
-    print(np.std(crrt_array))
-"""
+
